b943c034098c0ed012e7498b9d93b4f30149811d
Murch <shorukov.murat94@gmail.com>
2021-08-16 20:10:19 +0600

    added reverse str  method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("changes on my project");
+//        System.out.println("changes on my project");
+        String str = "Azamat";
+        System.out.println(reverse(str));
+    }
+
+    public static String reverse (String str) {
+        String reversed = "";
+        for( int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
     }
 }
