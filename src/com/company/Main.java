2b56ce6378e5757f9920e16cd9f1e13c067cab17
Bakusya <baktygulakimbekova@gmail.com>
2021-08-16 17:14:51 +0300

    commitOne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("changes on my project");
+        System.out.println(reverse("Bakusya"));
+
+    }
+
+    public static String reverse (String str){
+        String reversed = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            reversed += str.charAt(i);
+
+        }return reversed;
     }
 }
