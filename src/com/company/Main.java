bb71f0d42b8c1b83b58e7c6167ac8be0cc18bf8b
Bakusya <baktygulakimbekova@gmail.com>
2021-08-16 18:02:18 +0300

    polindromMethod

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("changes on my project");
         System.out.println(reverse("Bakusya"));
+        String str = "hello";
+        System.out.println(isPolendrom(str));
+
 
     }
 
@@ -14,5 +17,13 @@ public class Main {
             reversed += str.charAt(i);
 
         }return reversed;
+    }
+    public static boolean isPolendrom(String str){
+        String str1 = reverse(str);
+        if(str.equals(str1)){
+            return true;
+        }else {
+
+        }return false;
     }
 }
